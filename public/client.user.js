b18123e77f551d895e694bf52e7d118ff50ccfdc
Christopher Sharman <christopher.p.sharman@gmail.com>
2016-05-03 23:06:47 +0000

    Client version number update.

--- public/client.user.js
+++ public/client.user.js
@@ -2,7 +2,7 @@
 // @name		slither-friends
 // @author		Christopher Sharman
 // @namespace	https://slither-friends.csharman.co.uk
-// @version		0.0.6
+// @version		0.1
 // @description Slither with friends!
 // @downloadURL https://slither-friends.csharman.co.uk/client.user.js
 // @updateURL	https://slither-friends.csharman.co.uk/client.user.js
